df82a1bae7e9b2c8c89f9c82f78201a7e3b75fc1
Jeremy Apthorp <nornagon@nornagon.net>
2020-10-03 22:02:44 -0700

    fix selection in code

--- src/PageText.tsx
+++ src/PageText.tsx
@@ -9,7 +9,7 @@ import u from 'unist-builder'
 import visit from 'unist-util-visit';
 import wikiLink from 'remark-wiki-link';
 
-function renderMarkdownToHtml(text: string) {
+function pipeline() {
   return unified()
     .use(markdown)
     .use(wikiLink, {
@@ -20,37 +20,52 @@ function renderMarkdownToHtml(text: string) {
     .use(remark2rehype, {
       allowDangerousHtml: true,
       handlers: {
+        code(h, node) {
+          var value = node.value ? node.value + '\n' : ''
+          var lang = node.lang && (node.lang as any).match(/^[^ \t]+(?=[ \t]|$)/)
+          var props: any = {}
+        
+          if (lang) {
+            props.className = ['language-' + lang]
+          }
+          props['x-pos'] = node.position!.start.offset! + 4 + (lang?.length ?? 0)
+        
+          return h((node as any).position, 'pre', [h(node, 'code', props, [u('text', value)])])
+        },
+        inlineCode(h, node: any) {
+          var value = node.value.replace(/\r?\n|\r/g, ' ')
+          return h(node, 'code', {'x-pos': node.position?.start.offset + 1}, [u('text', value)])
+        },
         text(h, node) {
           return h.augment(
             node,
-            u('element', {tagName: 'span', properties: {'x-pos': node.position?.start.offset}}, [u('text', String(node.value).replace(/[ \t]*(\r?\n|\r)[ \t]*/g, '$1'))])
+            u('element',
+              {tagName: 'span', properties: {'x-pos': node.position?.start.offset}},
+              [u('text', String(node.value).replace(/[ \t]*(\r?\n|\r)[ \t]*/g, '$1'))])
           )
         }
       }
     })
     .use(raw)
     .use(stringify)
+}
+
+function renderMarkdownToHtml(text: string) {
+  return pipeline()
     .processSync(text)
     .toString('utf8')
 }
 
 export default function PageText({text}: {text: string}) {
   const html = useMemo(() => {
-    console.log(extractLinks(text))
+    console.log(pipeline().parse(text))
     return renderMarkdownToHtml(text)
   }, [text])
   return <div dangerouslySetInnerHTML={ { __html: html } } />
 }
 
 export function extractLinks(text: string): any[] {
-  const tree = unified()
-    .use(markdown)
-    .use(wikiLink, {
-      hrefTemplate: (link: string) => `/${link}`,
-      pageResolver: (name: string) => [name],
-      wikiLinkClassName: 'wikilink',
-    })
-    .parse(text)
+  const tree = pipeline().parse(text)
   const links: {href: string}[] = []
   visit(tree, 'wikiLink', (node) => {
     links.push({href: node.value as string})
