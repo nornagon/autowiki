239c2c9f8aac170f345ba308123c48bb21dd63c8
Jeremy Rose <nornagon@nornagon.net>
2020-10-31 13:09:44 -0700

    Updates

---
+++ precache-manifest.fc62832b7c4d30b4a7f322dde1678608.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4dd4c04a6fd0a6bd3161233d500248",
+    "url": "/autowiki/index.html"
+  },
+  {
+    "revision": "8b104d16b22e4f5f48c9",
+    "url": "/autowiki/static/css/2.1d5ea050.chunk.css"
+  },
+  {
+    "revision": "f78d7f6c6c36afe0feef",
+    "url": "/autowiki/static/css/main.723dd500.chunk.css"
+  },
+  {
+    "revision": "8b104d16b22e4f5f48c9",
+    "url": "/autowiki/static/js/2.11169ec9.chunk.js"
+  },
+  {
+    "revision": "1cdb8474ab3013cd738a38d38b454189",
+    "url": "/autowiki/static/js/2.11169ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78d7f6c6c36afe0feef",
+    "url": "/autowiki/static/js/main.36e050c0.chunk.js"
+  },
+  {
+    "revision": "51fab68b939e652ffc00",
+    "url": "/autowiki/static/js/runtime-main.311d4bf5.js"
+  }
+]);
